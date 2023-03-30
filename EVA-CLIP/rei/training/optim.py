46b041482a9f1fa4658254d035618f0e2a6b24d4
QuanSun <quansun84@gmail.com>
2023-03-30 13:32:02 +0800

    remove unused code in optim

--- EVA-CLIP/rei/training/optim.py
+++ EVA-CLIP/rei/training/optim.py
@@ -161,17 +161,6 @@ def get_assigner(args, model):
 
 def get_all_parameters(args, model):
     assigner_visual, assigner_text = get_assigner(args, model)
-    if (assigner_visual is None) and (assigner_text is None):
-        named_parameters = list(model.named_parameters())
-        exclude = lambda n, p: p.ndim < 2 or "bn" in n or "ln" in n or "bias" in n or 'logit_scale' in n
-        include = lambda n, p: not exclude(n, p)
-
-        gain_or_bias_params = [p for n, p in named_parameters if exclude(n, p) and p.requires_grad]
-        rest_params = [p for n, p in named_parameters if include(n, p) and p.requires_grad]
-        parameters = []
-        parameters.extend(gain_or_bias_params)
-        parameters.extend(rest_params)
-        return parameters
         
     parameters = []
     visual_parameters = get_parameters(args, model, assigner_visual, 'visual')
